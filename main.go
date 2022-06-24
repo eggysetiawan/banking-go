6540b7584d1b9d073d42351feb9bf5f19e0e26ff
Rahmat Setiawan <setiawaneggy@gmail.com>
2022-06-24 20:23:53 +0700

    json encoding

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name       string `json:"name"`
+	City       string `json:"city"`
+	PostalCode string `json:"postalCode"`
+}
+
 func main() {
 
 	// defines route
 	http.HandleFunc("/index", index)
+	http.HandleFunc("/customers", customer)
 
 	// starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
@@ -17,4 +25,15 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
+}
+
+func customer(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Rahmat", "Jakarta", "13460"},
+		{"Dani", "Kediri", "22450"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customers)
 }
